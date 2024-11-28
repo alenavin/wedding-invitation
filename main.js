6d9741eadd6ac3d03b496950964b9aa7873c20b8
Alena Vinogradova <alenb.v@yandex.ru>
2024-11-28 12:47:38 +0000

    form upd

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//const { default: axios } = require("axios");
+
 const days = document.querySelector("#days");
 const hours = document.querySelector("#hours");
 const min = document.querySelector("#min");
@@ -23,3 +25,39 @@ function update() {
 
 update();
 setInterval(update, 1000);
+
+///////////////////////////////////////////////////////////
+const TOKEN = "8083808487:AAHjc03EiXUOL8Z372s8ohu7JTr-gyLTYqw";
+const CHAT_ID = "-1002330677694";
+const URL_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+
+document
+  .getElementById("tg")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    let message = `<b>Подтверждение от гостя!</b>\n`;
+    message += `<b>Имя:</b> ${this.name.value}\n`;
+    message += `<b>Телефон:</b> ${this.phone.value}\n`;
+    message += `<b>Напитки:</b> ${this.drinks.value}\n`;
+    message += `<b>Трансфер:</b> ${this.transfer.value}`;
+    console.log(message);
+    const notification = document.getElementById("notification");
+
+    axios
+      .post(URL_API, {
+        chat_id: CHAT_ID,
+        parse_mode: "html",
+        text: message,
+      })
+      .then((res) => {
+        this.name.value = "";
+        this.phone.value = "";
+        this.drinks.value = "";
+        this.transfer.value = "";
+        notification.style.display = "block";
+      });
+  })
+  .catch((err) => {
+    console.warn(err);
+  })
+  .finally(() => {});
